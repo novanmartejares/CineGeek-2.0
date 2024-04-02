f168ba745f61012cc721f027d5bcb5fee238fedd
dr.tuxer <38743836+dragonpilee@users.noreply.github.com>
2024-04-02 15:12:51 +0530

    Update site.ts

--- src/configs/site.ts
+++ src/configs/site.ts
@@ -11,7 +11,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: env.NEXT_PUBLIC_SITE_NAME,
   author: env.NEXT_PUBLIC_SITE_NAME,
-  slogan: 'Watch TV Shows Online, Watch Movies Online.',
+  slogan: 'Watch TV Shows & Movies Online.',
   description:
     'Watch movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more.',
   keywords: [
